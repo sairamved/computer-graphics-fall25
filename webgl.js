1ee35671659ff002495116a53e148afaf94681e7
Sai Ram Ved Vijapurapu <sv2884@nyu.edu>
2025-10-08 00:27:40 -0400

    added hw4

--- webgl.js
+++ webgl.js
@@ -1,3 +1,13 @@
+let extraCode = `
+vec3  _s(vec3 i) { return cos(5.*(i+5.*cos(5.*(i.yzx+5.*cos(5.*(i.zxy+5.*cos(5.*i))))))); }
+float _t(vec3 i, vec3 u, vec3 a) { return dot(normalize(_s(i + a)), u - a); }
+float noise(vec3 p) {
+   vec3 i = floor(p), u = p - i, v = 2.*mix(u*u, u*(2.-u)-.5, step(.5,u));
+   return mix(mix(mix(_t(i, u, vec3(0.,0.,0.)), _t(i, u, vec3(1.,0.,0.)), v.x),
+                  mix(_t(i, u, vec3(0.,1.,0.)), _t(i, u, vec3(1.,1.,0.)), v.x), v.y),
+              mix(mix(_t(i, u, vec3(0.,0.,1.)), _t(i, u, vec3(1.,0.,1.)), v.x),
+                  mix(_t(i, u, vec3(0.,1.,1.)), _t(i, u, vec3(1.,1.,1.)), v.x), v.y), v.z);
+}`;
 function gl_start(canvas, scene) {
    setTimeout(function() {
       gl = canvas.getContext('webgl2');
@@ -12,7 +22,8 @@ function gl_start(canvas, scene) {
             gl.attachShader(gl.program, shader);
          };
          addshader(gl.VERTEX_SHADER, vertexShader);
-         addshader(gl.FRAGMENT_SHADER, fragmentShader);
+         let i = fragmentShader.indexOf('float') + 6;
+         addshader(gl.FRAGMENT_SHADER, fragmentShader.substring(0,i) + extraCode + fragmentShader.substring(i));
          gl.linkProgram(gl.program);
          if (! gl.getProgramParameter(gl.program, gl.LINK_STATUS))
             console.log('Could not link the shader program!');
@@ -32,5 +43,5 @@ function gl_start(canvas, scene) {
       }, 30);
    }, 100);
 }
-let gl;
+let gl, _ = {};
 let setUniform = (type,name,a,b,c) => (gl['uniform'+type])(gl.getUniformLocation(gl.program,name), a,b,c);
